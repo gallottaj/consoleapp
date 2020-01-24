c2d5e92cd5b1338832d49c3fe22495726ea908be
Joe Gallotta <joeygallotta@gmail.com>
2020-01-23 16:03:06 -0800

    create currentuser

--- Uno/src/UnoConsoleApp2.java
+++ Uno/src/UnoConsoleApp2.java
@@ -8,9 +8,11 @@ public class UnoConsoleApp2 {
 		ArrayList<UnoCard> playertwo = new ArrayList<UnoCard>();
 		ArrayList<UnoCard> playerthree = new ArrayList<UnoCard>();
 		ArrayList<UnoCard> playerfour = new ArrayList<UnoCard>();
+
 		Scanner input;
 		int cardIndex;
 		int winner = 0;
+		int currentuser;
 		UnoCard drawpileCard;
 		String currentColor;
 
@@ -25,123 +27,123 @@ public class UnoConsoleApp2 {
 			draw(7, playertwo);
 			draw(7, playerthree);
 			draw(7, playerfour);
+
 			System.out.println("The current card is: " + drawpileCard.getCard());
 
-			for (int currentplayer = 1; winner == 0;) {
+			for (currentuser = 1; winner == 0;) {
 				cardIndex = 0;
 
-				System.out.println("Player ONE, You're Up! " + String.valueOf(playerone.size()) + " cards left!"
-						+ ((playerone.size() == 1) ? "...Uno!" : ""));
-				// Finding a card to place
-				for (cardIndex = 0; cardIndex < playerone.size(); cardIndex++) {
-					if (((UnoCard) playerone.get(cardIndex)).canPlace(drawpileCard, currentColor)) // Searching for
-																									// playable
-																									// cards
-						break;
-				}
+				// player one's turn
 
-				if (cardIndex == playerone.size()) {
-					System.out.println("I've got nothing! Drawing cards...");
-					draw(1, playerone);
-				} else {
-					drawpileCard = (UnoCard) playerone.get(cardIndex);
-					playerone.remove(cardIndex);
-					currentColor = drawpileCard.color;
-					System.out.println("Player ONE Chooses" + drawpileCard.getCard() + "!");
-					System.out.println("You're Up Player TWO!");
-					currentplayer = 2;
+				if (currentuser == 1) {
 
-					// Must do as part of each turn because topCard can stay the same through a
-					// round
-					if (drawpileCard.value >= 10) {
-						currentplayer = 2;
-
-						switch (drawpileCard.value) {
-						case 12: // Draw 2
-							System.out.println("Player ONE must draw 2 cards");
-							draw(2, playerone);
+					System.out.println("Player One, You're Up! " + String.valueOf(playertwo.size()) + " cards left!"
+							+ ((playertwo.size() == 1) ? "...Uno!" : ""));
+					// Finding a card to place
+					for (cardIndex = 0; cardIndex < playerone.size(); cardIndex++) {
+						if (((UnoCard) playerone.get(cardIndex)).canPlace(drawpileCard, currentColor)) // Searching for
+																										// playable
+																										// cards
 							break;
-
-						case 13:
-						case 14: // Wild cards
-							do // Picking a random color that's not none
-							{
-								currentColor = new UnoCard().color;
-							} while (currentColor == "none");
-
-							System.out.println("I choose " + currentColor);
-							if (drawpileCard.value == 14) // Wild draw 4
-							{
-								System.out.println("Player ONE must draw 4 cards");
-								draw(4, playerone);
-							}
-						}
 					}
-				}
 
-				// PLAYER TWO
+					if (cardIndex == playerone.size()) {
+						System.out.println("I've got nothing! Drawing cards...");
+						draw(1, playerone);
+					} else {
+						drawpileCard = (UnoCard) playerone.get(cardIndex);
+						playerone.remove(cardIndex);
+						currentColor = drawpileCard.color;
+						System.out.println("Player One Chooses" + drawpileCard.getCard() + "!");
+						System.out.println("You're Up Player Two!");
+						currentuser = 2;
 
-				for (currentplayer = 2; winner == 0;) {
-					cardIndex = 0;
+						// Must do as part of each turn because topCard can stay the same through a
+						// round
+						if (drawpileCard.value >= 10) {
+//							playerOnesTurn = false; // Skipping turn
 
-					{
-						System.out.println("Player TWO Has" + " " + String.valueOf(playertwo.size()) + " cards left!"
-								+ ((playertwo.size() == 1) ? "...Uno!" : ""));
-						// Finding a card to place
-						for (cardIndex = 0; cardIndex < playertwo.size(); cardIndex++) {
-							if (((UnoCard) playertwo.get(cardIndex)).canPlace(drawpileCard, currentColor)) // Searching
-																											// for
-																											// playable
-																											// cards
+							switch (drawpileCard.value) {
+							case 12: // Draw 2
+								System.out.println("Drawing 2 cards for you...");
+								draw(2, playerone);
 								break;
-						}
 
-						if (cardIndex == playertwo.size()) {
-							System.out.println("I've got nothing! Drawing cards...");
-							draw(1, playertwo);
-						} else {
-							drawpileCard = (UnoCard) playertwo.get(cardIndex);
-							playertwo.remove(cardIndex);
-							currentColor = drawpileCard.color;
-							System.out.println("Player TWO Chooses" + drawpileCard.getCard() + "!");
-							System.out.println("You're Up Player THREE!");
-							currentplayer = 3;
+							case 13:
+							case 14: // Wild cards
+								do // Picking a random color that's not none
+								{
+									currentColor = new UnoCard().color;
+								} while (currentColor == "none");
 
-							// Must do as part of each turn because topCard can stay the same through a
-							// round
-							if (drawpileCard.value >= 10) {
-								currentplayer = 3;
-
-								switch (drawpileCard.value) {
-								case 12: // Draw 2
-									System.out.println("Drawing 2 cards for you...");
-									draw(2, playertwo);
-									break;
-
-								case 13:
-								case 14: // Wild cards
-									do // Picking a random color that's not none
-									{
-										currentColor = new UnoCard().color;
-									} while (currentColor == "none");
-
-									System.out.println("New color is " + currentColor);
-									if (drawpileCard.value == 14) // Wild draw 4
-									{
-										System.out.println("Drawing 4 cards for you...");
-										draw(4, playertwo);
-									}
-
+								System.out.println("New color is " + currentColor);
+								if (drawpileCard.value == 14) // Wild draw 4
+								{
+									System.out.println("Drawing 4 cards for you...");
+									draw(4, playerone);
 								}
 							}
+						}
 
-							// PLAYER THREE
+						// PLAYER TWO
 
-							for (currentplayer = 3; winner == 0;) {
-								cardIndex = 0;
+						if (currentuser == 2)
+
+						{
+							System.out.println("Player Two Has" + " " + String.valueOf(playertwo.size())
+									+ " cards left!" + ((playertwo.size() == 1) ? "...Uno!" : ""));
+							// Finding a card to place
+							for (cardIndex = 0; cardIndex < playertwo.size(); cardIndex++) {
+								if (((UnoCard) playertwo.get(cardIndex)).canPlace(drawpileCard, currentColor)) // Searching
+																												// for
+																												// playable
+																												// cards
+									break;
+							}
+
+							if (cardIndex == playertwo.size()) {
+								System.out.println("I've got nothing! Drawing cards...");
+								draw(1, playertwo);
+							} else {
+								drawpileCard = (UnoCard) playertwo.get(cardIndex);
+								playertwo.remove(cardIndex);
+								currentColor = drawpileCard.color;
+								System.out.println("Player Two Chooses" + drawpileCard.getCard() + "!");
+								System.out.println("You're Up Player Three!");
+								currentuser = 3;
+
+								// Must do as part of each turn because topCard can stay the same through a
+								// round
+								if (drawpileCard.value >= 10) {
+									currentuser = 3; // Skipping turn
+
+									switch (drawpileCard.value) {
+									case 12: // Draw 2
+										System.out.println("Drawing 2 cards for you...");
+										draw(2, playertwo);
+										break;
+
+									case 13:
+									case 14: // Wild cards
+										do // Picking a random color that's not none
+										{
+											currentColor = new UnoCard().color;
+										} while (currentColor == "none");
+
+										System.out.println("New color is " + currentColor);
+										if (drawpileCard.value == 14) // Wild draw 4
+										{
+											System.out.println("Drawing 4 cards for you...");
+											draw(4, playertwo);
+										}
+
+									}
+								}
+
+								if (currentuser == 3)
 
 								{
-									System.out.println("Player THREE Has" + " " + String.valueOf(playerthree.size())
+									System.out.println("Player Three Has" + " " + String.valueOf(playerthree.size())
 											+ " cards left!" + ((playerthree.size() == 1) ? "...Uno!" : ""));
 									// Finding a card to place
 									for (cardIndex = 0; cardIndex < playerthree.size(); cardIndex++) {
@@ -159,14 +161,14 @@ public class UnoConsoleApp2 {
 										drawpileCard = (UnoCard) playerthree.get(cardIndex);
 										playerthree.remove(cardIndex);
 										currentColor = drawpileCard.color;
-										System.out.println("Player THREE Chooses" + drawpileCard.getCard() + "!");
-										System.out.println("You're Up Player FOUR!");
-										currentplayer = 4;
+										System.out.println("Player Three Chooses" + drawpileCard.getCard() + "!");
+										System.out.println("You're Up Player four!");
+										currentuser = 4;
 
 										// Must do as part of each turn because topCard can stay the same through a
 										// round
 										if (drawpileCard.value >= 10) {
-											currentplayer = 4;
+											currentuser = 4; // Skipping turn
 
 											switch (drawpileCard.value) {
 											case 12: // Draw 2
@@ -189,85 +191,96 @@ public class UnoConsoleApp2 {
 												}
 
 											}
+
 										}
 
-										// PLAYER FOUR
+										if (currentuser == 4) {
 
-										for (currentplayer = 4; winner == 0;) {
-											cardIndex = 0;
+											System.out.println("Player Four, You're Up! "
+													+ String.valueOf(playerfour.size()) + " cards left!"
+													+ ((playerfour.size() == 1) ? "...Uno!" : ""));
+											// Finding a card to place
+											for (cardIndex = 0; cardIndex < playerfour.size(); cardIndex++) {
+												if (((UnoCard) playerfour.get(cardIndex)).canPlace(drawpileCard,
+														currentColor)) // Searching for
+																		// playable
+																		// cards
+													break;
+											}
 
-											{
-												System.out.println("Player FOUR Has" + " "
-														+ String.valueOf(playerfour.size()) + " cards left!"
-														+ ((playerfour.size() == 1) ? "...Uno!" : ""));
-												// Finding a card to place
-												for (cardIndex = 0; cardIndex < playerfour.size(); cardIndex++) {
-													if (((UnoCard) playerfour.get(cardIndex)).canPlace(drawpileCard,
-															currentColor)) // Searching
-																			// for
-																			// playable
-																			// cards
+											if (cardIndex == playerfour.size()) {
+												System.out.println("I've got nothing! Drawing cards...");
+												draw(1, playerfour);
+											} else {
+												drawpileCard = (UnoCard) playerfour.get(cardIndex);
+												playerone.remove(cardIndex);
+												currentColor = drawpileCard.color;
+												System.out
+														.println("Player Four Chooses" + drawpileCard.getCard() + "!");
+												System.out.println("You're Up Again Player One!");
+												currentuser = 1;
+
+												// Must do as part of each turn because topCard can stay the same
+												// through a
+												// round
+
+												if (drawpileCard.value >= 10) {
+													currentuser = 1; // Skipping turn
+
+													switch (drawpileCard.value) {
+													case 12: // Draw 2
+														System.out.println("Drawing 2 cards for you...");
+														draw(2, playerfour);
 														break;
-												}
 
-												if (cardIndex == playerfour.size()) {
-													System.out.println("I've got nothing! Drawing cards...");
-													draw(1, playerfour);
-												} else {
-													drawpileCard = (UnoCard) playerfour.get(cardIndex);
-													playerfour.remove(cardIndex);
-													currentColor = drawpileCard.color;
-													System.out.println(
-															"Player FOUR Chooses" + drawpileCard.getCard() + "!");
-													System.out.println("You're Up Player ONE!");
-													currentplayer = 1;
+													case 13:
+													case 14: // Wild cards
+														do // Picking a random color that's not none
+														{
+															currentColor = new UnoCard().color;
+														} while (currentColor == "none");
 
-													// Must do as part of each turn because topCard can stay the same
-													// through a
-													// round
-													if (drawpileCard.value >= 10) {
-														currentplayer = 1;
-
-														switch (drawpileCard.value) {
-														case 12: // Draw 2
-															System.out.println("Drawing 2 cards for you...");
-															draw(2, playerfour);
-															break;
-
-														case 13:
-														case 14: // Wild cards
-															do // Picking a random color that's not none
-															{
-																currentColor = new UnoCard().color;
-															} while (currentColor == "none");
-
-															System.out.println("New color is " + currentColor);
-															if (drawpileCard.value == 14) // Wild draw 4
-															{
-																System.out.println("Drawing 4 cards for you...");
-																draw(4, playerfour);
-															}
-
+														System.out.println("New color is " + currentColor);
+														if (drawpileCard.value == 14) // Wild draw 4
+														{
+															System.out.println("Drawing 4 cards for you...");
+															draw(4, playerfour);
 														}
 													}
 												}
 											}
+											// If decks are empty
+											if (playerone.size() == 0)
+												winner = 1;
+											if (playertwo.size() == 0)
+												winner = 2;
+											if (playerthree.size() == 0)
+												winner = 3;
+											if (playerfour.size() == 0)
+												winner = 4;
+
+											// detect a winner
+
+											if (winner == 1)
+												System.out.println("Player one wins!");
+											if (winner == 2)
+												System.out.println("player two wins!");
+											if (winner == 3)
+												System.out.println("Player three wins!");
+											if (winner == 4)
+												System.out.println("player four wins!");
 										}
+
 									}
 
 								}
 
 							}
-
 						}
 					}
+
 				}
 			}
-
-			if (winner == 1)
-				System.out.println("playerwins");
-			else
-				System.out.println("player two wins");
 
 		}
 	}
